bb0e101e5f4216a4df8e87b79f795e898c090d10
ottovbismarck <fritzj96@gmail.com>
2018-04-09 20:34:30 -0700

    refactored the console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,2 @@
-console.log('I\'m in this class primarily based on Terry\'s advice that I need to be more patient. I think this will be good practice.');
+var myHistory = 'I\'m in this class primarily based on Terry\'s advice that I need to be more patient. I think this will be good practice.'
+console.log(myHistory);
